2e94b4b6e56a646ba69fd22c538752318617f4e5
Chimoha31 <mokochii1108@gmail.com>
2021-11-18 19:53:27 -0800

    First custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
+import "../components/ExpenseItem.css";
 
- const ExpenseItem = () => {
-  return(
-    <>
-    <h2>Expense item</h2>
-    </>
-  )
-}
+function ExpenseItem(props) {
+  const { title, amount, date } = props;
+
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
