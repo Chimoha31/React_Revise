942659a82be5b572459c97ab12188af0d0680e5b
Chimoha31 <mokochii1108@gmail.com>
2022-01-20 04:16:55 -0800

    Use map to output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,8 +5,8 @@ import ExpenseFilter from "../Expenses/ExpenseFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
-  const { items } = props;
-  const [filteredYear, setFilteredYear] = useState('2020');
+  // const { items } = props;
+  const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -14,27 +14,17 @@ const Expenses = (props) => {
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={items[0].title}
-          amount={items[0].amount}
-          date={items[0].date}
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={items[1].title}
-          amount={items[1].amount}
-          date={items[1].date}
-        />
-        <ExpenseItem
-          title={items[2].title}
-          amount={items[2].amount}
-          date={items[2].date}
-        />
-        <ExpenseItem
-          title={items[3].title}
-          amount={items[3].amount}
-          date={items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
